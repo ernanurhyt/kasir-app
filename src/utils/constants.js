1556d1d0cf769b0af609d24cd60c4d1e1ccba168
ernanurhyt <166375796+ernanurhyt@users.noreply.github.com>
2026-01-16 19:48:26 +0700

    Add files via upload

---
+++ src/utils/constants.js
@@ -0,0 +1,3 @@
+export const API_URL = "https://backend-production-49fd.up.railway.app";
+// export const API_URL = "https://fake-server-zum.herokuapp.com/"
+//export const API_URL = "https://api-food-recipe.herokuapp.com/"
