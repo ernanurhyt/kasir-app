87ca0a39c082b90bf47fbda7c56a9dab75c78bf6
ernanurhyt <166375796+ernanurhyt@users.noreply.github.com>
2026-01-16 18:24:04 +0700

    Delete src directory

--- src/components/Index.js
+++
@@ -1,6 +0,0 @@
-import ComNavbar from './ComNavbar'
-import ComLeftBar from './ComLeftBar'
-import ComRightBar from './ComRightBar'
-import Menus from './Menus'
-
-export {ComNavbar, ComLeftBar, ComRightBar, Menus}
