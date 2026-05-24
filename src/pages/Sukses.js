8aa50fab9e9fdc6258119070b2edfea33c4992dd
ernanurhyt <166375796+ernanurhyt@users.noreply.github.com>
2026-05-24 23:31:13 +0700

    change to localhost

--- src/pages/Sukses.js
+++ src/pages/Sukses.js
@@ -13,7 +13,7 @@ export default class Sukses extends Component {
           const keranjangs = res.data;
          keranjangs.map(function(item){
              return axios
-                    .delete("https://web-production-80a65.up.railway.app/keranjangs/"+item.id)
+                    .delete("http://localhost:8000/keranjangs/"+item.id)
                     .then((res) => console.log(res))
                     .catch((error) => console.log(error))
          })
